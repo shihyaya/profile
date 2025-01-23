8135230009eda5d285060c4db4134ccd439f0d34
shihyaya <frand2303s@gmail.com>
2025-01-23 21:17:57 +0800

    basePath

--- next.config.mjs
+++ next.config.mjs
@@ -7,8 +7,8 @@ const nextConfig = {
   output: 'export',
   poweredByHeader: false,
   trailingSlash: true,
-  // basePath: '/idw-intro', // 加上去在，dev開發模式會有問題
-  // assetPrefix: '/idw-intro/', // 加上去在，dev開發模式會有問題
+  basePath: '/profile', // 加上去在，dev開發模式會有問題
+  // assetPrefix: '/profile', // 加上去在，dev開發模式會有問題
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
