54cddd1f9a9475bbdbc46e68f9cf999f2185195b
shihyaya <frand2303s@gmail.com>
2025-01-23 18:09:56 +0800

    調整config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,18 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
+  eslint: {
+    dirs: ['.'],
   },
-  assetPrefix: '/profile',
-  basePath: '/profile',
-  output: 'export'
+  output: 'export',
+  poweredByHeader: false,
+  trailingSlash: true,
+  // basePath: '/idw-intro', // 加上去在，dev開發模式會有問題
+  // assetPrefix: '/idw-intro/', // 加上去在，dev開發模式會有問題
+  // The starter code load resources from `public` folder with `router.basePath` in React components.
+  // So, the source code is "basePath-ready".
+  // You can remove `basePath` if you don't need it.
+  reactStrictMode: true,
 };
 
 export default nextConfig;
