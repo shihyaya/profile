1be214a4f2c45974ca675cc46a3a5757aa6b389d
shihyaya <frand2303s@gmail.com>
2025-01-23 17:32:03 +0800

    1. gh-page 調整css增加檔案 2. 調整文字

--- pages/template/experiences.tsx
+++ pages/template/experiences.tsx
@@ -20,7 +20,7 @@ const exData=[{
     startDate: '2019/04',
     endDate:'2021/04',
     describe: '1. 網頁規劃與製作：根據不同活動製作與規劃網頁與圖片。<br>2. 協助網站平台轉移及APP規劃：網頁的規劃與轉移，亦包含會員轉移的活動規劃協助。<br>3. 美編設計：網路圖片與活動頁設計、FB廣告圖與EDM製作。<br>4. O2O導流活動規劃與協助：協助將客人導流至門市體驗，並與門市協同合作導流至線上購買',
-    skills: '使用技能：Angular、Html、Javascript、Css/Sass、Figma'
+    skills: '使用技能：Adobe Illustrator、Adobe Photoshop'
 },]
 export default function Experiences() {
     return (
